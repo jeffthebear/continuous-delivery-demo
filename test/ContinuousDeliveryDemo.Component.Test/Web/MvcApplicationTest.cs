fbfc51fae351bb61e4fde13ee88e5841fefb46f0
Jeff Hsiung <jhsiung@gmail.com>
2014-09-04 08:13:44 -0700

    Added test for mvc application start

---
+++ test/ContinuousDeliveryDemo.Component.Test/Web/MvcApplicationTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using ContinuousDeliveryDemo.Component.Test.Fakes;
+using ContinuousDeliveryDemo.Component.Test.Helpers;
+using ContinuousDeliveryDemo.Infrastructure.Redis;
+using ContinuousDeliveryDemo.Web;
+using ContinuousDeliveryDemo.Web.Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ContinuousDeliveryDemo.Component.Test.Web
+{
+    [TestClass]
+    public class MvcApplicationTest
+    {
+        [TestMethod]
+        public void Application_StartShouldInitialize()
+        {
+            // Arrange
+            RedisConnection.RedisConnectionStringProviderOverride = new FakeRedisConnectionStringProvider();
+            dynamic mvcApplicationWrapper = new AccessPrivateWrapper(new MvcApplication());
+
+            // Act
+            mvcApplicationWrapper.Application_Start();
+
+            // Assert
+            Assert.IsInstanceOfType(DependencyResolver.Current, typeof(UnityMvcDependencyResolver));
+            Assert.IsInstanceOfType(ModelBinders.Binders.DefaultBinder, typeof(UnityMvcModelBinder));
+        }
+    }
+}
